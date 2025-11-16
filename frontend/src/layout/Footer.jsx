7a444e53317a35659b341498028b67ce64f18cc5
Shlesha5847 <147795373+Shlesha5847@users.noreply.github.com>
2025-11-16 12:09:47 +0530

    Commit

--- frontend/src/layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const Footer = () => (
-  <footer className="bg-gray-800 text-white text-center py-3">
-    © {new Date().getFullYear()} SpiceBite Restaurant. All rights reserved.
-  </footer>
-);
-
-export default Footer;
